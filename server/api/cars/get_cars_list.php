d8770c1700667e9db63a969bc2a807f6f23b0c4f
Michał Marchewczyk <dev@marchewczyk.eu>
2021-12-05 01:30:12 +0100

    Fix amount of people waiting in car info

--- server/api/cars/get_cars_list.php
+++ server/api/cars/get_cars_list.php
@@ -7,7 +7,8 @@ require '../users/guard_is_moderator.php';
 
 $stmt = $db->prepare("SELECT cars.id, car_models.id as model_id, year, mileage, color, availability, price,
     make, model, body_type, number_of_seats, power, transmission, COUNT(reservations.id) as count FROM cars
-    LEFT JOIN car_models on car_models.id = cars.model_id LEFT JOIN reservations on cars.id = reservations.car_id
+    LEFT JOIN car_models on car_models.id = cars.model_id 
+    LEFT JOIN reservations on cars.id = reservations.car_id AND (reservations.status = 'WAITING' OR reservations.status = 'CONFIRMED')
     GROUP BY cars.id");
 
 if (!$stmt) {
