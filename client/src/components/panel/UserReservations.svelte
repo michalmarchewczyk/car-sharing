5cbbb6fdf157d8ffe7288dfcadf8b6e064963105
Michał Marchewczyk <dev@marchewczyk.eu>
2021-12-06 17:54:19 +0100

    Add status filter to user reservations list

--- client/src/components/panel/UserReservations.svelte
+++ client/src/components/panel/UserReservations.svelte
@@ -5,6 +5,10 @@ import UserReservationsItem from './UserReservationsItem.svelte';
 
 const location = useLocation();
 
+let filter = '';
+
+$: filteredReservations = filter ? $userReservations.filter(r => r.status === filter) : $userReservations;
+
 </script>
 
 <div class="flex-1 flex flex-col max-w-sm max-h-full">
@@ -14,10 +18,19 @@ const location = useLocation();
             <a href='/panel/reservations' class="button-inline">
                 View all <span class="material-icons top-0 relative float-right ml-1">arrow_forward</span>
             </a>
+        {:else}
+            <select bind:value={filter}>
+                <option value="">-</option>
+                <option value="WAITING">WAITING</option>
+                <option value="CONFIRMED">CONFIRMED</option>
+                <option value="CANCELLED">CANCELLED</option>
+                <option value="ACTIVE">ACTIVE</option>
+                <option value="DONE">DONE</option>
+            </select>
         {/if}
     </h2>
     <div class="pb-4 rounded-lg mt-4 flex flex-col w-full gap-4 flex-1 overflow-y-auto px-2">
-        {#each $location.pathname === '/panel' ? $userReservations.slice(0,6) : $userReservations as reservation}
+        {#each $location.pathname === '/panel' ? $userReservations.slice(0,6) : filteredReservations as reservation}
             <UserReservationsItem reservation={reservation}/>
         {/each}
     </div>
@@ -27,5 +40,9 @@ const location = useLocation();
 <style lang="scss">
   .button-inline {
 	@apply inline-block relative mx-3 bg-blue-700 text-white p-1.5 px-2 rounded-md hover:bg-blue-800 cursor-pointer text-base font-bold;
+  }
+  select{
+	@apply top-0 bg-white w-32 border-2 border-gray-600 focus:border-blue-600 h-10 rounded-md text-base my-2 disabled:opacity-50 px-1 float-right mr-6;
+	margin-top: -0.5rem;
   }
 </style>
