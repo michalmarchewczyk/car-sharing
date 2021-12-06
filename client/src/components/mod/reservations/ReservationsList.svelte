7209e134686a836b5c49bf6fe979c54690c12948
Michał Marchewczyk <dev@marchewczyk.eu>
2021-12-06 17:52:12 +0100

    Add status filter to reservations list

--- client/src/components/mod/reservations/ReservationsList.svelte
+++ client/src/components/mod/reservations/ReservationsList.svelte
@@ -6,6 +6,10 @@
 
     const location = useLocation();
 
+    let filter = '';
+
+    $: filteredReservations = filter ? $reservations.filter(r => r.status === filter) : $reservations;
+
     onMount(async () => {
         await fetchReservations();
     });
@@ -20,10 +24,19 @@
             <a href='/mod/reservations' class="button-inline">
                 View all <span class="material-icons top-0 relative float-right ml-1">arrow_forward</span>
             </a>
+        {:else}
+            <select bind:value={filter}>
+                <option value="">-</option>
+                <option value="WAITING">WAITING</option>
+                <option value="CONFIRMED">CONFIRMED</option>
+                <option value="CANCELLED">CANCELLED</option>
+                <option value="ACTIVE">ACTIVE</option>
+                <option value="DONE">DONE</option>
+            </select>
         {/if}
     </h2>
-    <div class="overflow-y-auto flex-1 px-2 pb-2">
-        {#each $location.pathname === '/mod' ? $reservations.slice(0,5) : $reservations as reservation}
+    <div class="overflow-y-scroll flex-1 px-2 pb-2">
+        {#each $location.pathname === '/mod' ? $reservations.slice(0,5) : filteredReservations as reservation}
             <ReservationsListItem reservation={reservation}/>
         {:else}
             <div class="h-24 pt-12 flex-1 rounded-xl pb-0 max-w-3xl text-3xl font-bold text-gray-400 text-center " style="min-width: 20rem">
@@ -44,5 +57,9 @@
   }
   .button {
 	@apply block text-center bg-blue-700 text-white p-2 px-4 rounded-md hover:bg-blue-800 cursor-pointer text-lg font-bold mt-3;
+  }
+  select{
+	@apply top-0 bg-white w-32 border-2 border-gray-600 focus:border-blue-600 h-10 rounded-md text-base my-2 disabled:opacity-50 px-1 float-right mr-6;
+    margin-top: -0.5rem;
   }
 </style>
